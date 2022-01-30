473044a858bcd6d38c5a39f90466691ce7c9ba46
Finn Ellis <finn@finn.fun>
2022-01-30 01:17:51 -0800

    tabs to spaces

--- just-hack.js
+++ just-hack.js
@@ -1,4 +1,4 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	await ns.hack(ns.args[0]);
+  await ns.hack(ns.args[0]);
 }
--- just-weaken.js
+++ just-weaken.js
@@ -1,4 +1,4 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	await ns.weaken(ns.args[0]);
+  await ns.weaken(ns.args[0]);
 }
