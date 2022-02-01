3957e22b42b5f4b83f4e031cbbc7443c292bdc60
Finn Ellis <finn@finn.fun>
2022-01-31 23:28:27 -0800

    slightly more precision

--- doCrimes.js
+++ doCrimes.js
@@ -1,18 +1,18 @@
 /** @param {NS} ns **/
 
 const crimes = [
-  "shoplift",
-  "rob store",
-  "mug someone",
-  "larceny",
-  "deal drugs",
-  "bond forgery",
-  "traffick illegal arms",
-  "homicide",
-  "grand theft auto",
-  "kidnap and ransom",
-  "assassinate",
-  "heist",
+    "shoplift",
+    "rob store",
+    "mug someone",
+    "larceny",
+    "deal drugs",
+    "bond forgery",
+    "traffick illegal arms",
+    "homicide",
+    "grand theft auto",
+    "kidnap and ransom",
+    "assassinate",
+    "heist",
 ];
 
 function getCrimeValues(ns, priority) {
@@ -32,12 +32,21 @@ export async function main(ns) {
     // possible priorities: karma, kills, money, STAT_exp, xp
     const priority = ns.args[0] || "money";
     const crimeValues = getCrimeValues(ns, priority);
+    const ev = priority.endsWith("xp") ?
+        // you get half xp for failing a crime
+        (crime) => {
+            const chance = ns.getCrimeChance(crime);
+            return (crimeValues[crime] * chance) +
+                (crimeValues[crime]/2 * (1 - chance));
+        } :
+        // other rewards only happen if you succeed
+        (crime) => crimeValues[crime] * ns.getCrimeChance(crime);
     while (true) {
         // do this inside the loop because success chance changes
-        crimes.sort((a, b) =>
-            crimeValues[b] * ns.getCrimeChance(b) - crimeValues[a] * ns.getCrimeChance(a));
+        crimes.sort((a, b) => ev(b) - ev(a));
         // if we don't keep tail up it's hard to stop this script
         ns.tail();
+        ns.print(`Expected rate: ${ev(crimes[0])*1000} ${priority}/s`);
         await ns.commitCrime(crimes[0]);
         while (ns.isBusy()) {
             await ns.sleep(10);
